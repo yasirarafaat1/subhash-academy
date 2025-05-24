f415cbf96be5ba77f8cbbc918f8e0003ec0af67a
yasirarafaat1 <mailforyasir@gmail.com>
2025-05-25 01:11:16 +0530

    Gallery Updated

--- lib/data.ts
+++ lib/data.ts
@@ -66,76 +66,419 @@ export const facilities: Facility[] = [
 export const galleryImages: GalleryImage[] = [
   {
     id: "1",
-    src: "/assets/infrastructure/multimedia.JPG",
-    alt: "gallery",
-    width: 2071,
-    height: 1380
-  },
-  {
-    id: "9",
-    src: "/assets/infrastructure/lab.png",
+    src: "/assets/gallery/A.JPG",
     alt: "gallery",
     width: 2071,
     height: 1380
   },
   {
     id: "2",
+    src: "/assets/gallery/B.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "3",
+    src: "/assets/gallery/C.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "4",
+    src: "/assets/gallery/D.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "5",
+    src: "/assets/gallery/DSC_3984.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "6",
+    src: "/assets/gallery/DSC_3987.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "7",
+    src: "/assets/gallery/DSC_3992.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "8",
+    src: "/assets/gallery/DSC_4007.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "9",
+    src: "/assets/gallery/DSC_4008.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "10",
+    src: "/assets/gallery/DSC_4026.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "11",
+    src: "/assets/gallery/DSC_4062.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "12",
+    src: "/assets/gallery/DSC_4064.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "13",
+    src: "/assets/gallery/farewell/farewell.jpg",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "14",
+    src: "/assets/gallery/farewell/farewell2.jpg",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "15",
+    src: "/assets/gallery/farewell/IMG-20220823-WA0046.jpg",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "16",
+    src: "/assets/gallery/farewell/IMG-20250217-WA0000.jpg",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "17",
+    src: "/assets/gallery/farewell/IMG-20250224-WA0004.jpg",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "18",
+    src: "/assets/gallery/farewell/IMG-20250224-WA0050.jpg",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "19",
+    src: "/assets/gallery/farewell/DSC_2884.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "20",
+    src: "/assets/gallery/DSC_4082.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "21",
+    src: "/assets/gallery/DSC_4088.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "22",
+    src: "/assets/gallery/DSC_4108.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "23",
+    src: "/assets/gallery/DSC_4115.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "24",
+    src: "/assets/gallery/DSC_4117.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "25",
+    src: "/assets/gallery/DSC_4155 (2).JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "26",
+    src: "/assets/gallery/DSC_4190.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "27",
+    src: "/assets/gallery/DSC_4191.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "28",
+    src: "/assets/gallery/DSC_4197.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "29",
+    src: "/assets/gallery/DSC_4201 (2).JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "30",
+    src: "/assets/gallery/DSC_4228.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "31",
+    src: "/assets/gallery/DSC_4257.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "32",
+    src: "/assets/gallery/DSC_4317.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "33",
+    src: "/assets/gallery/DSC_4342.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "34",
+    src: "/assets/gallery/DSC_4348.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "35",
+    src: "/assets/gallery/DSC_4445.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "36",
+    src: "/assets/gallery/DSC_4458.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "37",
+    src: "/assets/gallery/DSC_4459.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "38",
+    src: "/assets/gallery/DSC_4464.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "39",
+    src: "/assets/gallery/DSC_4470.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "40",
+    src: "/assets/gallery/DSC_4474.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "41",
+    src: "/assets/gallery/DSC_4479.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "42",
+    src: "/assets/gallery/DSC_4481.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "43",
+    src: "/assets/gallery/DSC_4483.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "44",
+    src: "/assets/gallery/DSC_4484.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "45",
+    src: "/assets/gallery/DSC_4486.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "46",
+    src: "/assets/gallery/DSC_4490.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "47",
+    src: "/assets/gallery/DSC_4493.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "48",
+    src: "/assets/gallery/DSC_4498.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "49",
+    src: "/assets/gallery/DSC_4504.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "50",
+    src: "/assets/gallery/DSC_4515.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "51",
+    src: "/assets/infrastructure/multimedia.JPG",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "52",
+    src: "/assets/infrastructure/lab.png",
+    alt: "gallery",
+    width: 2071,
+    height: 1380
+  },
+  {
+    id: "53",
     src: "/assets/gallery/farewell.jpg",
     alt: "gallery",
     width: 2072,
     height: 1384
   },
   {
-    id: "10",
-    src : "/assets/infrastructure/class.jpeg",
+    id: "54",
+    src: "/assets/infrastructure/class.jpeg",
     alt: "gallery",
     width: 2072,
     height: 1384
   },
   {
-    id: "3",
+    id: "55",
     src: "/assets/infrastructure/library.jpeg",
     alt: "gallery",
     width: 2070,
     height: 1380
   },
   {
-    id: "4",
-    src: "/assets/gallery/national_ocassion.jpg",
+    id: "56",
+    src: "/assets/gallery/img4.jpg",
     alt: "gallery",
     width: 2070,
     height: 1380
   },
   {
-    id: "7",
-    src: "/assets/gallery/img4.jpg",
-    alt: "",
-    width: 2070,
-    height: 1380
-  },
-  {
-    id: "8",
+    id: "57",
     src: "/assets/gallery/img5.jpg",
     alt: "gallery",
     width: 2074,
     height: 1384
   },
   {
-    id: "9",
+    id: "58",
     src: "/assets/gallery/img6.jpg",
     alt: "gallery",
     width: 2072,
     height: 1384
   },
   {
-    id: "10",
+    id: "59",
     src: "/assets/gallery/farewell2.jpg",
     alt: "gallery",
     width: 2070,
     height: 1380
   },
   {
-    id: "11",
+    id: "60",
     src: "/assets/gallery/img8.jpg",
     alt: "gallery",
     width: 2070,
